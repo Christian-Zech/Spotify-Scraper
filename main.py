6b6521e81f3186d328634d17a24859960d0df407
Christian <christianjzech@gmail.com>
2023-12-30 19:57:42 -0600

    Added keys for youtube part of the project.

--- main.py
+++ main.py
@@ -9,10 +9,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-client_id = "85c374790a0d4f119b2ade28c37931e0"
-redirectUri = 'http://localhost:8080'
+spotify_client_id = "85c374790a0d4f119b2ade28c37931e0"
+spotify_redirectUri = 'http://localhost:8080'
+spotify_authUrl = "https://accounts.spotify.com/authorize"
 
-authUrl = "https://accounts.spotify.com/authorize"
+youtube_key = "AIzaSyDEC6EKIdE-03qbBZlrH_7fkmURvDBocQQ"
+youtube_client_auth= "136009703112-907m04u5ptovfo9qoqgfuta8okfcc0kh.apps.googleusercontent.com"
 driver = webdriver.Chrome()
 
 
@@ -27,34 +29,34 @@ def base64_encode(hash):
     return base64_encoded.decode().replace('=', '')
 
 
-def get_token():
+def get_spotify_token():
     code_verifier = generate_random_string(64)
     hashed = sha256_hash(code_verifier)
     code_challenge = base64_encode(hashed)
     
     params = {
         "response_type": 'code',
-        "client_id": client_id,
+        "client_id": spotify_client_id,
         "code_challenge_method": 'S256',
         "code_challenge": code_challenge,
-        "redirect_uri": redirectUri,
+        "redirect_uri": spotify_redirectUri,
     }
     
     authorization_url = "https://accounts.spotify.com/authorize?" + urllib.parse.urlencode(params)
     driver.get(authorization_url)
-    uri_present = EC.url_contains(redirectUri)
+    uri_present = EC.url_contains(spotify_redirectUri)
     WebDriverWait(driver, 9999).until(uri_present)
     authorization_code = urllib.parse.urlparse(driver.current_url).query.split('=')[1]
     driver.quit()
-    print("Logging in...")
+    print("Loading tracks from playlist...")
     
     #webbrowser.open(authorization_url, new=1, autoraise=True)
 
     token_request_data = {
         "grant_type": "authorization_code",
         "code": authorization_code,
-        "redirect_uri": redirectUri,
-        "client_id": client_id,
+        "redirect_uri": spotify_redirectUri,
+        "client_id": spotify_client_id,
         "code_verifier": code_verifier,
     }
     
@@ -71,14 +73,18 @@ def get_playlist(token, id):
 
 
 def main():
-    token = get_token()
+    token = get_spotify_token()
     # input("Enter playlist url: ")
     id = "https://open.spotify.com/playlist/4kROaFuLfVxaYSZT3WbJzw".split('/')[-1].split('?')[0]
     track_objects = get_playlist(token, id)["items"]
     tracks = [track_objects[i]["track"]["name"] for i in range(len(track_objects))]
     
-    print(tracks)
+    print("Songs loaded:")
+    for track in tracks:
+        print(track)
     
+    
+    print("Searching youtube...")
     
 
 
